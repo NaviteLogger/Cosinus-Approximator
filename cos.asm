fa8bb0b667422ab86c3c7fcdd3af24db2c9f62b2
NaviteLogger <kacprzakmarek92@gmail.com>
2025-01-02 10:27:23 +0000

    Initial commit

---
+++ cos.asm
@@ -0,0 +1,83 @@
+global cos_asm
+global silnia
+global power
+
+%idefine x [ebp + 8] ; float is 2 byte
+%idefine n [ebp + 12] ; int is 1 byte
+
+cos_asm:
+	push ebp
+	mov ebp, esp
+
+    mov ecx, n
+
+    finit ; Init FPU
+    fld1 ; push 1 (initial return value)
+
+    mov edx, 0
+
+    jmp cos_loop
+
+cos_loop:
+    ; st0 = pow(x, n)/ n! + st0
+    cmp ecx, 0
+    je return
+
+    call power ; fpu: pow(x, ecx), n!
+    call silnia ; fpu: ecx!
+
+    fdivp ; pow(x, n)/n!
+
+    faddp ; st0  = st0 + pow(x, n)/n!
+
+    dec ecx
+    jmp cos_loop
+
+return:
+    leave
+    jmp raw_ret
+
+power:
+    push ecx
+    fld1 ; initial return value
+
+    call power_loop
+
+    pop ecx
+    ret
+
+power_loop:
+    cmp ecx, 0
+    jle raw_ret
+
+    fld dword x
+    fmulp
+
+    dec ecx
+    jmp power_loop
+
+silnia:
+    push ecx
+
+    fld1 ; initial return value
+
+    call silnia_loop
+
+    pop ecx
+    ret
+
+silnia_loop:
+    cmp ecx, 1
+    jle raw_ret
+
+    push ecx
+    fild dword [esp]
+    pop ecx
+
+    fmulp ; (prev + 1)*prev
+
+    dec ecx
+    jmp silnia_loop
+
+raw_ret:
+    ret
---
+++ cos.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+// Każdy projekt powinien posiadać interfejs napisany
+// w języku C (wprowadzanie danych z klawiatury, wypisywanie
+// ich na ekran, nieskończona pętla programu itp.) oraz mieć
+// logikę biznesową napisaną w asemblerze z wykorzystaniem
+// FPU. Dodatkowo, każdy projekt powinien móc przyjąć
+// dodatkowy argument w postaci liczby miejsc po przecinku, do
+// której ma być zaokrąglony wynik. Liczbę tę podaje użytkownik
+// z klawiatury.
+// Napisać program, który oblicza e^x, gdzie x jest
+// argumentem podanym przez użytkownika.
+
+#define MAX_VAL(type) ((type)((1U << (sizeof(type) * 8 - 1)) - 1))
+
+void clearInvalidInput() {
+  while (getchar() != '\n')
+    ;
+}
+
+extern float cos_asm(float x, int accuracy) asm(
+    "cos_asm"); // explicitly tell GCC name of the asm function to use
+
+int main() {
+  printf("Cosinus calculator");
+  do {
+    long double x;
+    unsigned int accuracy, isInputValid;
+    printf("\nArgument: ");
+    isInputValid = scanf("%Lf", &x);
+
+    if (isInputValid == 0) {
+      printf("Couldn't parse x!\n");
+      clearInvalidInput();
+      continue;
+    } else if (x < 0 || x > MAX_VAL(float)) {
+      printf("Value of x must be greater than 0 and less than %f\n",
+             MAX_VAL(float));
+      continue;
+    }
+
+    printf("Input accuracy (number of taylor series terms)\n");
+    printf("Value: ");
+    isInputValid = scanf("%d", &accuracy);
+
+    if (isInputValid == 0) {
+      printf("Couldn't parse accuracy!\n");
+      clearInvalidInput();
+      continue;
+    } else if (accuracy < 0 || accuracy > MAX_VAL(unsigned int)) {
+      printf("Accuracy must be greater than 0 and less than %d\n",
+             MAX_VAL(unsigned int));
+      continue;
+    }
+
+    float result = cos_asm(x, accuracy);
+    printf("e^x = ~%.*f\n", accuracy, result);
+
+    printf("Continue? (y/n) ");
+    char c;
+    scanf(" %c", &c);
+    if (c != 'y' && c != 'Y') {
+      break;
+    }
+  } while (1);
+
+  return 0;
+}
